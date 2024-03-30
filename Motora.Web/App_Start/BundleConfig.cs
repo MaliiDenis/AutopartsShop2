318251e2dd2853ea9cb3943bfaa8272360a2a813
denis.malii <denis.malii@isa.utm.md>
2024-03-31 01:38:58 +0200

    Add bundle config

---
+++ Motora.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Motora.Web
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Create a new script bundle for JavaScript files
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/jquery-3.4.1.min.js",
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/custom.js"));
+
+            // Create a new style bundle for CSS files
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
+                "~/Content/css/bootstrap.css",
+                "~/Content/css/font-awesome.min.css",
+                "~/Content/css/style.css",
+                "~/Content/css/responsive.css"));
+        }
+    }
+}
