8cc323e6b34cc602ff9bdb43d43a9843a1456eb4
denis.malii <denis.malii@isa.utm.md>
2024-04-12 16:34:25 +0300

    Add db context

---
+++ Motora.Database/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using Motora.Domain.Models;
+
+namespace Motora.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+            : base("SqlServer")
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        public DbSet<WarehouseStock> WarehouseStocks { get; set; }
+    }
+}
