9e7b8bdd088c6348b650873c096cb89fbbecfdff
denis.malii <denis.malii@isa.utm.md>
2024-04-12 17:22:36 +0300

    add migrations

---
+++ Motora.Database/Migrations/202404281731549_start.cs
@@ -0,0 +1,152 @@
+﻿namespace Motora.Database.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class start : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Brands",
+                c => new
+                    {
+                        BrandId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.BrandId);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        SKU = c.String(),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Description = c.String(),
+                        BrandId = c.Int(nullable: false),
+                        CategoryId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .ForeignKey("dbo.Brands", t => t.BrandId, cascadeDelete: true)
+                .ForeignKey("dbo.Categories", t => t.CategoryId, cascadeDelete: true)
+                .Index(t => t.BrandId)
+                .Index(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        CategoryId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.CategoryId);
+            
+            CreateTable(
+                "dbo.ProductFeatures",
+                c => new
+                    {
+                        ProductFeatureId = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        FeatureName = c.String(),
+                        FeatureValue = c.String(),
+                    })
+                .PrimaryKey(t => t.ProductFeatureId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        OrderDetailId = c.Int(nullable: false, identity: true),
+                        OrderId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderDetailId)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.OrderId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        OrderId = c.Int(nullable: false, identity: true),
+                        OrderDate = c.DateTime(nullable: false),
+                        CustomerId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.OrderId)
+                .ForeignKey("dbo.Customers", t => t.CustomerId, cascadeDelete: true)
+                .Index(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.Customers",
+                c => new
+                    {
+                        CustomerId = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Address = c.String(),
+                        ContactNumber = c.String(),
+                    })
+                .PrimaryKey(t => t.CustomerId);
+            
+            CreateTable(
+                "dbo.WarehouseStocks",
+                c => new
+                    {
+                        WarehouseStockId = c.Int(nullable: false, identity: true),
+                        WarehouseId = c.Int(nullable: false),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.WarehouseStockId)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .ForeignKey("dbo.Warehouses", t => t.WarehouseId, cascadeDelete: true)
+                .Index(t => t.WarehouseId)
+                .Index(t => t.ProductId);
+            
+            CreateTable(
+                "dbo.Warehouses",
+                c => new
+                    {
+                        WarehouseId = c.Int(nullable: false, identity: true),
+                        Location = c.String(),
+                    })
+                .PrimaryKey(t => t.WarehouseId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.WarehouseStocks", "WarehouseId", "dbo.Warehouses");
+            DropForeignKey("dbo.WarehouseStocks", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.OrderDetails", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.OrderDetails", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "CustomerId", "dbo.Customers");
+            DropForeignKey("dbo.ProductFeatures", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "CategoryId", "dbo.Categories");
+            DropForeignKey("dbo.Products", "BrandId", "dbo.Brands");
+            DropIndex("dbo.WarehouseStocks", new[] { "ProductId" });
+            DropIndex("dbo.WarehouseStocks", new[] { "WarehouseId" });
+            DropIndex("dbo.Orders", new[] { "CustomerId" });
+            DropIndex("dbo.OrderDetails", new[] { "ProductId" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderId" });
+            DropIndex("dbo.ProductFeatures", new[] { "ProductId" });
+            DropIndex("dbo.Products", new[] { "CategoryId" });
+            DropIndex("dbo.Products", new[] { "BrandId" });
+            DropTable("dbo.Warehouses");
+            DropTable("dbo.WarehouseStocks");
+            DropTable("dbo.Customers");
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.ProductFeatures");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Products");
+            DropTable("dbo.Brands");
+        }
+    }
+}
