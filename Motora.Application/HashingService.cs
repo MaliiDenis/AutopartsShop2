d29ca67a502a320636c08e90ed4f9244414d5fe7
denis.malii <denis.malii@isa.utm.md>
2024-04-28 13:41:31 +0300

    Implement hashing service

---
+++ Motora.Application/HashingService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Motora.Application
+{
+    public static class HashingService
+    {
+        public static string CreateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                provider.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = String.Concat(password, salt);
+                byte[] saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                byte[] hashBytes = sha256.ComputeHash(saltedPasswordBytes);
+            
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public static bool VerifyPassword(string enteredPassword, string storedHash, string storedSalt)
+        {
+            var hashOfEntered = HashPassword(enteredPassword, storedSalt);
+            return hashOfEntered.Equals(storedHash);
+        }
+    }
+}
