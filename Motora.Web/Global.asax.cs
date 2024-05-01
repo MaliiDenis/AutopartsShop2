fa03c3ef2d1ca8409f31320144df207ee09b6fd5
denis.malii <denis.malii@isa.utm.md>
2024-05-01 12:26:49 +0300

    Add master page

--- Motora.Web/Global.asax.cs
+++ Motora.Web/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace Motora.Web
 {
@@ -16,6 +18,27 @@ namespace Motora.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+        
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                try
+                {
+                    FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                    if (authTicket != null && !authTicket.Expired)
+                    {
+                        var roles = authTicket.UserData.Split(',');
+                        HttpContext.Current.User = new GenericPrincipal(new FormsIdentity(authTicket), roles);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    FormsAuthentication.SignOut();
+                }
+            }
         }
     }
 }
