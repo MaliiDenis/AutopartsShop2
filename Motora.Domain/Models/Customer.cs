86ff18c1a06c3819977b7562d6aacfaaa9b401de
denis.malii <denis.malii@isa.utm.md>
2024-05-12 10:35:42 +0300

    Add admin controller

--- Motora.Domain/Models/Customer.cs
+++ Motora.Domain/Models/Customer.cs
@@ -8,6 +8,11 @@ namespace Motora.Domain.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public string ContactNumber { get; set; }
+        
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string Role { get; set; }
 
         // Навигационное свойство
         public ICollection<Order> Orders { get; set; } = new HashSet<Order>();
---
+++ Motora.Web/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using Motora.Database;
+
+namespace Motora.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Admin/Feedbacks
+        public ActionResult Feedbacks()
+        {
+            var feedbacks = _context.Feedbacks.ToList();
+            return View(feedbacks);
+        }
+
+        // POST: Admin/DeleteFeedback/5
+        [HttpPost]
+        public ActionResult DeleteFeedback(int id)
+        {
+            var feedback = _context.Feedbacks.Find(id);
+            if (feedback != null)
+            {
+                _context.Feedbacks.Remove(feedback);
+                _context.SaveChanges();
+                return RedirectToAction("Feedbacks");
+            }
+
+            return HttpNotFound();
+        }
+    }
+}
